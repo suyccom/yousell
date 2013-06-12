98aea7de9d7c54b31150b1bda1f6788d984fcfff
Ibon Castilla Varela <ibon.castilla@gmail.com>
2013-06-12 14:03:52 +0200

    First load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yousell::Application.config.secret_token = 'd64a484727121eb0256eef3743ed7cc1bcd55f2c33f43b545142a5800e209717fdeb691242bc51a34d9e273c6790389110ab4e573fc62f61ec22f48289a71ea9'
