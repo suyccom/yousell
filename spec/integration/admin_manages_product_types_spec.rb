9605fec1d7d03726b6946aae8a5712ed970f44d5
Ibon Castilla Varela <ibon.castilla@gmail.com>
2013-06-28 11:53:42 +0200

    Fix test

--- spec/integration/admin_manages_product_types_spec.rb
+++ spec/integration/admin_manages_product_types_spec.rb
@@ -14,14 +14,14 @@ feature 'The admin wants to manage product_types', :driver => :selenium do
     fill_in 'product_type[name]', :with => 'Barra de pan'
     fill_in 'product_type[default_price]', :with => '1'
     click_on 'Create Product type'
-    page.find('tr.product_type:nth-child(1) .this-view').should have_content 'Barra de pan'
+    page.find('tr.product_type:nth-child(1) .name-view').should have_content 'Barra de pan'
     page.find('tr.product_type:nth-child(1) .default-price-view').should have_content '1'
     # The user can edit a product_type (no conditions by now)
     page.find('tr.product_type:nth-child(1) .icon-edit').click
     fill_in 'product_type[name]', :with => 'Barra'
     fill_in 'product_type[default_price]', :with => '1.5'
     click_on 'Save Product type'
-    page.find('tr.product_type:nth-child(1) .this-view').should have_content 'Barra'
+    page.find('tr.product_type:nth-child(1) .name-view').should have_content 'Barra'
     page.find('tr.product_type:nth-child(1) .default-price-view').should have_content '1.5'
     # The user can add another product_type
     click_on 'New Product type'
@@ -29,7 +29,7 @@ feature 'The admin wants to manage product_types', :driver => :selenium do
     fill_in 'product_type[default_price]', :with => '2'
     click_on 'Create Product type'
     page.should have_css("tr.product_type", :count => 2)
-    page.find('tr.product_type:nth-child(2) .this-view').should have_content 'Pan de pueblo'
+    page.find('tr.product_type:nth-child(2) .name-view').should have_content 'Pan de pueblo'
     page.find('tr.product_type:nth-child(2) .default-price-view').should have_content '2'
     # The user can't delete a product_type if it's been being used on a product_type_product_type
     pending("We can't test product_type deletion until the application has stock")
