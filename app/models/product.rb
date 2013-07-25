495504b39ee9bbe3cc130722813a549ddfe42615
Ibon Castilla Varela <ibon.castilla@gmail.com>
2013-07-25 13:28:20 +0200

    * Provider code must be required, so barcode could be automatically generated. * Barcode could not be editable in any way by the user (edit and new views). * Test.

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,30 +7,38 @@ class Product < ActiveRecord::Base
     price   :decimal, :precision => 8, :scale => 2, :default => 0
     amount  :integer
     barcode :string, :unique
-    provider_code :string
+    provider_code :string, :required
     timestamps
   end
   attr_accessible :price, :amount, :barcode, :product_type, :product_type_id, :product_variations, :provider_code, :provider, :provider_id
 
+  # --- Relations --- #
   belongs_to :product_type
   belongs_to :provider
 
   has_many :product_variations, :accessible => true
   has_many :variations, :through => :product_variations
-  
+
+  # --- Validations --- #
+  validates_presence_of :provider
   validates :provider_code, :allow_blank => true, :uniqueness => {
     :link => Proc.new{|f| 
       Rails.application.routes.url_helpers.product_path(Product.find_by_provider_code(f[:value]))},
     :provider_code => Proc.new{|f| f[:value] }
   }
-  
+
+  # --- Callbacks --- #
   before_save :set_name
   def set_name
     self.name = "#{product_type} #{product_variations.*.value.join(' ')}"
   end
 
-  # --- Permissions --- #
+  before_save :auto_barcode
+  def auto_barcode
+    self.barcode = self.provider.code + self.provider_code
+  end
 
+  # --- Permissions --- #
   def create_permitted?
     acting_user.administrator?
   end
