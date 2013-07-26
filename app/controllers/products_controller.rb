c95f154e048ba3485ba4a60ffa3a1f23be9e51a3
Ignacio Huerta <ignacio@ihuerta.net>
2013-07-26 12:38:16 +0200

    Finish label printing for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,20 +21,45 @@ class ProductsController < ApplicationController
   end
   
   def print_labels
+    @product = Product.find(params[:id])
+
+    temp_file = "#{Rails.root}/tmp/barcode.png"
+    temp_pdf = "#{Rails.root}/tmp/labels.pdf"
+   
+    # Create the barcode PNG
     require 'barby'
     require 'barby/barcode/code_128'
     require 'barby/outputter/png_outputter'
+    barby = Barby::Code128B.new(@product.barcode)
+    png = Barby::PngOutputter.new(barby).to_png(:height => 20, :margin => 5)
+    File.open(temp_file, 'w'){|f| f.write png }
     
-    barcode = Barby::Code128B.new('4561231')
-    png = Barby::PngOutputter.new(barcode).to_png(:height => 20, :margin => 5)
-    File.open('tmp/barcode.png', 'w'){|f| f.write png }
-
-    names = ["", "Jordan", "Chris", "Jon", "Mike", "Kelly", "Bob", "Greg"]
-    labels = Prawn::Labels.render(names, :type => "Avery5160") do |pdf, name|
-      pdf.text name
-      pdf.image "#{Rails.root}/tmp/barcode.png" unless name.blank?
+    # Create the array for the labels PDF
+    barcodes = []
+    params[:empty_cells].to_i.times do
+      barcodes << ""
     end
-    send_data labels, :filename => "names.pdf", :type => "application/pdf", :disposition => "inline"
+    params[:number].to_i.times do
+      barcodes << temp_file
+    end
+    
+    # Generate the PDF
+    labels = Prawn::Labels.generate(temp_pdf, barcodes, :type => "Avery5160") do |pdf, barcode|
+      unless barcode.blank?
+        pdf.image barcode 
+        pdf.text @product.barcode
+        pdf.text @product.name
+      end
+    end
+    
+    # Print or send the PDF back to the browser
+    if defined? PRINT_LABELS_COMMAND
+      system("#{PRINT_LABELS_COMMAND} #{temp_pdf}")
+      flash[:info] = I18n.t("product.show.labels_sent_to_printer")
+      redirect_to @product
+    else
+      send_file temp_pdf, :type => "application/pdf", :disposition => "inline"
+    end
   end
 
 end
