e2e5d184a45bbbf9ec6d39f2db1175b769eb9661
Ibon Castilla Varela <ibon.castilla@gmail.com>
2013-07-29 00:52:29 +0200

    Fix migrations

--- db/migrate/20130721233228_hobo_migration_16_daysale.rb
+++
@@ -1,9 +0,0 @@
-class HoboMigration16Daysale < ActiveRecord::Migration
-  def self.up
-    add_column :sales, :sale_total, :decimal, :precision => 8, :scale => 2, :default => 0
-  end
-
-  def self.down
-    remove_column :sales, :sale_total
-  end
-end
