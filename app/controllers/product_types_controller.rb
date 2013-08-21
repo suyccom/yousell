50e7c9da16584c5fd493f6e4ffc2a0cf95d7f5d6
Imanol Alvarez <imanol@alvarezperez.net>
2013-08-21 11:01:47 +0200

    allow line break

--- app/controllers/product_types_controller.rb
+++ app/controllers/product_types_controller.rb
@@ -28,12 +28,13 @@ class ProductTypesController < ApplicationController
     if params[:products_transfer] && !params[:products_transfer].empty?
       from = Warehouse.find_by_name(params[:from]).id
       to = Warehouse.find_by_name(params[:to]).id
-      for barcode in params[:products_transfer].split(',')
+      for b in params[:products_transfer].gsub("\r\n", ",").split(',')
+        
         # Rest one to the quantity of product in the warehouse indicated
-        init_pr = ProductWarehouse.where('warehouse_id = ?',from).where('product_id = ?', Product.find_by_barcode(barcode))
+        init_pr = ProductWarehouse.where('warehouse_id = ?',from).where('product_id = ?', Product.find_by_barcode(b))
         init_pr.first.update_attribute(:amount, init_pr.first.amount - 1)
         # I'm adding to the amount of product in stock destination
-        end_pr = ProductWarehouse.where('warehouse_id = ?',to).where('product_id = ?', Product.find_by_barcode(barcode))
+        end_pr = ProductWarehouse.where('warehouse_id = ?',to).where('product_id = ?', Product.find_by_barcode(b))
         end_pr.first.update_attribute(:amount, end_pr.first.amount + 1)
         flash[:message] = "Se han cambiado los productos de almacen"
       end
