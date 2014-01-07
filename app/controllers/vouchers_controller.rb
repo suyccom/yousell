c93cfa0bfafb420be74ba8c9f20d8cdaf2eb7106
Imanol Alvarez <imanol@alvarezperez.net>
2014-01-07 18:59:15 +0100

    poder imprimir un vale de descuento

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -5,9 +5,18 @@ class VouchersController < ApplicationController
   auto_actions :all, :except => :show
 
   def index
+    if params[:print]
+      @voucher = Voucher.find(params[:id])
+      render :pdf => I18n.t('activerecord.attributes.voucher.Voucher'), 
+        :show_as_html => params[:debug].present?, 
+        :encoding => 'UTF-8',
+        :disable_javascript => true,
+        :use_xserver => true,
+        :template => 'vouchers/voucher.pdf.dryml'
+    else
       hobo_index Voucher.apply_scopes(
       :state_is => params[:state],
     ), :per_page => params[:per_page] ? params[:per_page].to_i : 15
+    end
   end
-
 end
