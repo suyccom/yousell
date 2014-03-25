42aea6270b55cf14d96ea3e767a1ec8bd5f89346
Imanol Alvarez <imanol@alvarezperez.net>
2014-03-25 10:19:09 +0100

    solo permitir tener un vale de descuento

--- app/controllers/payment_methods_controller.rb
+++ app/controllers/payment_methods_controller.rb
@@ -4,4 +4,14 @@ class PaymentMethodsController < ApplicationController
 
   auto_actions :all, :except => :show
 
+
+  def update
+    if params[:payment_method] && params[:payment_method][:voucher].to_i > 0
+      for pm in PaymentMethod.all
+        pm.update_attribute(:voucher, false)
+      end
+    end
+    hobo_update
+  end
+
 end
