12f1cb5e7a9bb5067f6b46b142eaec7bc3fa81e5
Ibon Castilla Varela <ibon.castilla@gmail.com>
2014-11-19 11:21:04 +0100

    Set-to-production details

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,3 @@
 # Application configuration constants
 DOCUMENTATION_PATH = Rails.root.join('app','documentation').to_s
 TICKETS_PATH = Rails.root.join(DOCUMENTATION_PATH,'tickets').to_s
-PRINT_LABELS_COMMAND = "lp "
-PRINT_TICKETS_COMMAND = "lp -o cpi=18 -o lpi=10 "
-SHOP_NAME = 'Shop name'
